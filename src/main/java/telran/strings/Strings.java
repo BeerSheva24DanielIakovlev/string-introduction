af0f6dc423766d61e44313a965b2984b87fdcf94
DaniilJustCode <gromdan90@gmail.com>
2024-07-29 15:47:18 +0300

    homework to 30.07.24

--- src/main/java/telran/strings/Strings.java
+++ src/main/java/telran/strings/Strings.java
@@ -4,13 +4,30 @@ public class Strings {
     public static String firstName() {
         //regex for strings starting with capital letter and rest as lowercase letters
         //minimal length is 5 letters
-        return "pizdets" + "[A-Z][a-z]{4,}";
+        return "[A-Z][a-z]{4,}";
     }
 
     public static String javaVariable() {
-        //TODO
         //regular expression for testing syntax of Java variable names
-        //only ASCII symbols are allowed
-        return "";
+        //only ASCII symbols are allowed 
+        
+        return "^(?!.*\\b("+ ServiceWords() +")\\b)[A-Za-z_$][A-Za-z0-9_$]*$";
+    }
+
+    public static String ServiceWords() {
+        String serviceWords = "";
+        String[] service_words = {"abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class",
+        "const", "continue", "default", "do", "double", "else", "enum", "extends", "final",
+        "finally", "float", "for", "goto", "if", "implements", "import", "instanceof", "int",
+        "interface", "long", "native", "new", "null", "package", "private", "protected",
+        "public", "return", "short", "static", "strictfp", "super", "switch", "synchronized",
+        "this", "throw", "throws", "transient", "try", "void", "volatile", "while", "true", "false"};
+        for (int i = 0; i < service_words.length - 1; i++) {
+            serviceWords += service_words[i] + "|";       
+        }
+        
+        serviceWords += service_words[service_words.length - 1];
+        
+        return serviceWords;
     }
 }
--- src/test/java/telran/strings/RegularExpressionsTest.java
+++ src/test/java/telran/strings/RegularExpressionsTest.java
@@ -1,8 +1,26 @@
 package telran.strings;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+import static telran.strings.Strings.*;
+
 public class RegularExpressionsTest {
+    @Test
     void javaVariableTest() {
-        //TODO
-        int 123;
+        String[] trueStrings = {"VasFm$", "a99999", "$$$", "Testing"};
+        String[] falseStrings = {"9ujk", "", "@#$%^&*", "KNU9**", "boolean"};
+        String regex = javaVariable();
+        for (int i = 0; i < trueStrings.length; i++) {
+            assertTrue(trueStrings[i].matches(regex));
+        }
+
+        for (int i = 0; i < falseStrings.length; i++) {
+            assertFalse(falseStrings[i].matches(regex));
+        }
     } 
+
+
 }
